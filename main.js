ac2d77acdc11c506b606bb567562fdee01202f68
yuyu <hcsd7b34@gmail.com>
2022-07-07 18:17:21 +0900

    Add: メインウインドウを実装

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const { env } = require('process');
+
+let mainWindow;
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1500,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload/main.js'),
+    },
+  });
+
+  require('./ipc/ipc.js')(mainWindow);
+
+  if (env.TYPE == 'debug') {
+    //デバッグ時のみデバッグメニューを表示
+    mainWindow.setMenu(Menu.buildFromTemplate([
+      ...require('./menu.js')(mainWindow),
+      {
+        label: 'デバッグ',
+        submenu: [
+          { label: 'デベロッパーツール', role: 'toggleDevTools' },
+          { label: '再読み込み', role: 'reload' }
+        ]
+      }
+    ]));
+  }
+  else mainWindow.setMenu(null);
+
+  mainWindow.loadFile('renderer/main/index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createMainWindow);
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createMainWindow();
+  }
+});
