7f448ad6a6a7aade758d1026f9b6de09e813870d
yuyu <hcsd7b34@gmail.com>
2022-07-09 10:20:18 +0900

    Style: 検索ボックスを追加

---
+++ renderer/main/js/tab.js
@@ -0,0 +1,17 @@
+export let displayedTab = null;
+
+export function createTab() {
+  displayedTab = document.getElementById('tabs').appendChild(document.createElement('div'));
+
+  //検索ボックス・検索ボタンを追加
+  let searchArea = document.createElement('div'); //Flexを使うためdivの中に入れる
+  searchArea.classList.add('search-area')
+  let searchBoxEl = document.createElement('input');
+  searchBoxEl.type = 'text';
+  searchBoxEl.classList.add('search-box');
+  searchArea.appendChild(searchBoxEl);
+  let searchButtonEl = document.createElement('button');
+  searchButtonEl.classList.add('search-button');
+  searchArea.appendChild(searchButtonEl)
+  displayedTab.appendChild(searchArea);
+}
---
+++ renderer/main/main.js
@@ -0,0 +1,3 @@
+import { createTab } from "./js/tab.js";
+
+createTab();
